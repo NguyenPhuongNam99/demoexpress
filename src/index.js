ac1b59c390e2058947cb0f12e5ca4d668c1942ec
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2021-08-15 02:23:29 +0700

    form submit with param in methor Post-Get

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const handlebars  = require('express-handlebars');
 
 const app = express();
 
-//HTTP logger
-app.use(morgan('combined'))
+//HTTP logger -log xem server cos chajy hay ko , xem trinh duyet gui request
+// app.use(morgan('combined'))
 //load anh 
 app.use(express.static(path.join(__dirname, 'public')))
 //Template engine
@@ -18,7 +18,7 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-console.log(__dirname);
+// console.log(__dirname);
 //C:\Users\namn3\blogExpress\src
 
 // app.engine('handlebars', exphbs());
@@ -29,6 +29,14 @@ app.get("/news", (req, res) => {
   return res.render('news')
 });
 
+app.get('/search',(req,res) =>{
+  console.log("res",req.query);
+  return res.render('search');
+})
+
+app.post('/search',(req,res)=>{
+  return res.render('search');
+})
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
