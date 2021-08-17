495a5add220f70bfe39a7179cbb472f8c3f79423
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2021-08-17 23:17:52 +0700

    mn

--- src/routers/Sites.js
+++ src/routers/Sites.js
@@ -5,5 +5,5 @@ const site = require('../app/controlers/SiteControler');
 
 router.use("/home",site.index)
 router.use("/search",site.search);
-
+// router.use("/search",site.search);
 module.exports = router;
